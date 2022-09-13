f5895c969bbc95d8c239423272d62579380abcdc
Niema Moshiri <niemamoshiri@gmail.com>
2022-09-13 12:48:35 -0700

    Added BA model

--- plugins/contact_network/__init__.py
+++ plugins/contact_network/__init__.py
@@ -2,5 +2,6 @@
 from .. import *
 from . import ngg
 PLUGIN_FUNCTIONS = {
+    "Barabasi-Albert (BA)": ngg.ngg_barabasi_albert,
     "Complete": ngg.ngg_complete,
 }
--- plugins/contact_network/ngg.py
+++ plugins/contact_network/ngg.py
@@ -2,11 +2,20 @@
 from .. import *
 from subprocess import call
 
-# simulate a complete graph using ngg_complete
-def ngg_complete(params, out_fn, verbose=True):
-    command = ['ngg_complete', str(params['n'])]
+# simulate contact network using NiemaGraphGen
+def ngg(exe, params, out_fn, verbose=True):
+    if exe == 'ngg_barabasi_albert':
+        command = ['ngg_barabasi_albert', str(params['n']), str(params['m'])]
+    elif exe == 'ngg_complete':
+        command = ['ngg_complete', str(params['n'])]
+    else:
+        error("Invalid NiemaGraphGen exe: %s" % exe)
     if verbose:
         print_log("Command: %s" % ' '.join(command))
     f = open(out_fn['contact_network'], 'w'); call(command, stdout=f); f.close()
     if verbose:
         print_log("Contact Network written to: %s" % out_fn['contact_network'])
+def ngg_barabasi_albert(params, out_fn, verbose=True):
+    ngg("ngg_barabasi_albert", params, out_fn, verbose=verbose)
+def ngg_complete(params, out_fn, verbose=True):
+    ngg("ngg_complete", params, out_fn, verbose=verbose)
