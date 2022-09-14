2de06de7c86204c13c61f5eccdf5ffeb8443476e
Niema Moshiri <niemamoshiri@gmail.com>
2022-09-14 14:03:02 -0700

    Added models

--- plugins/viral_phylogeny_seeds/__init__.py
+++ plugins/viral_phylogeny_seeds/__init__.py
@@ -2,6 +2,7 @@
 from .. import *
 from . import common_treesap
 PLUGIN_FUNCTIONS = {
+    "Coalescent (Neutral)": common_treesap.treesap_coalescent_const_pop_tree,
     "Dual-Birth": common_treesap.treesap_dualbirth,
     "None": DUMMY_PLUGIN_FUNC,
     "Yule": common_treesap.treesap_yule,
