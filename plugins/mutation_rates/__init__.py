4405a81d9afedab974fdfec9606dacbe7ad2cb44
Niema Moshiri <niemamoshiri@gmail.com>
2022-09-14 15:29:02 -0700

    Added models

--- plugins/mutation_rates/__init__.py
+++ plugins/mutation_rates/__init__.py
@@ -3,6 +3,7 @@ from .. import *
 from . import common_treeswift
 PLUGIN_FUNCTIONS = {
     "Autocorrelated Constant Increment": common_treeswift.treeswift_autocorr_const_inc,
+    "Autocorrelated Exponential": common_treeswift.treeswift_autocorr_exp,
     "Constant": common_treeswift.treeswift_constant,
     "None": DUMMY_PLUGIN_FUNC,
 }
--- plugins/mutation_rates/common_treeswift.py
+++ plugins/mutation_rates/common_treeswift.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python3
 from .. import *
+from numpy.random import exponential
 from random import random
 from treeswift import read_tree_newick
 
-# Autocorrelated Constant Increment mutation rates
+# Autocorrelated Constant Increment
 def treeswift_autocorr_const_inc(params, out_fn, config, verbose=True):
     tree = read_tree_newick(out_fn['viral_phylogeny_time'])
     p = params['p']; p_over_2 = p/2; delta = params['delta']; R_min = params['R_min']; R_max = params['R_max']
@@ -26,7 +27,21 @@ def treeswift_autocorr_const_inc(params, out_fn, config, verbose=True):
     if verbose:
         print_log("Viral Phylogeny (Mutations) written to: %s" % out_fn['viral_phylogeny_mut'])
 
-# Constant mutation rates
+# Autocorrelated Exponential
+def treeswift_autocorr_exp(params, out_fn, config, verbose=True):
+    tree = read_tree_newick(out_fn['viral_phylogeny_time'])
+    for node in tree.traverse_preorder():
+        if node.is_root():
+            node.rate = params['R0']
+        else:
+            node.rate = exponential(scale=node.parent.rate)
+        if node.edge_length is not None:
+            node.edge_length *= node.rate
+    tree.write_tree_newick(out_fn['viral_phylogeny_mut'])
+    if verbose:
+        print_log("Viral Phylogeny (Mutations) written to: %s" % out_fn['viral_phylogeny_mut'])
+
+# Constant
 def treeswift_constant(params, out_fn, config, verbose=True):
     tree = read_tree_newick(out_fn['viral_phylogeny_time']); r = params['rate']
     for node in tree.traverse_preorder():
