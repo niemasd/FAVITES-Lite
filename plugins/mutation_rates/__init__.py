0a7b80c0c0c49b9095fe4429750d9d20f343b316
Niema Moshiri <niemamoshiri@gmail.com>
2022-09-14 15:56:46 -0700

    Added models

--- plugins/mutation_rates/__init__.py
+++ plugins/mutation_rates/__init__.py
@@ -13,5 +13,6 @@ PLUGIN_FUNCTIONS = {
     "Gamma": common_treeswift.treeswift_gamma,
     "Inverse Gaussian (Wald)": common_treeswift.treeswift_inverse_gaussian,
     "Log-Normal": common_treeswift.treeswift_lognorm,
+    "Noncentral Chi-Squared": common_treeswift.treeswift_noncentral_chisq,
     "None": DUMMY_PLUGIN_FUNC,
 }
