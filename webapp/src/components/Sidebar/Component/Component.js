d4e60307d6878e02c7a9492627fe3903e1add756
jenetic <jznnylam@gmail.com>
2022-11-28 23:36:57 -0800

    Change sidebar component color to yellow ('in progress') when user chooses model

--- webapp/src/components/Sidebar/Component/Component.js
+++ webapp/src/components/Sidebar/Component/Component.js
@@ -1,20 +1,14 @@
 import styles from "./Component.module.css";
 import { useSelector, useDispatch } from "react-redux";
-// import { setConfig } from "../../../features/config/configSlice";
 
 const Component = (props) => {
   const { name, onClick, className } = props;
   const config = useSelector((state) => state.config.value);
-  const selected = useSelector((state) => state.selected.value);
-
-  // console.log(config);
-  // console.log(selected);
   
   return (
-    <div className={`${className} ${styles.Main}`} onClick={onClick}>
+    <div className={`${className} ${styles.Main} ${config[name] && styles.InProgress}`} onClick={onClick}>
       <h4>{name}</h4>
-      {
-        name in config ?
+      {name in config ?
         <span>{config[name]["model"]}</span> :
         <span>Not Selected</span>
       }
