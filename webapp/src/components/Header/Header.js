d85f836107d816632a5b321f5c15ce6362898da2
jenetic <jznnylam@gmail.com>
2023-01-18 01:02:30 -0800

    Add button to download as JSON

--- webapp/src/components/Header/Header.js
+++ webapp/src/components/Header/Header.js
@@ -11,6 +11,17 @@ import {
 import { useSelector } from "react-redux";
 import styles from "./Header.module.css";
 
+
+const downloadJson = (config) => {
+  const str = JSON.stringify(config);
+  const bytes = new TextEncoder().encode(str);
+  const blob = new Blob([bytes], {
+      type: "application/json;charset=utf-8"
+  });
+  const href = URL.createObjectURL(blob);
+  return href;
+}
+
 const Header = (props) => {
   const config = useSelector((state) => state.config.value);
   const [openExport, setOpenExport] = useState(false);
@@ -71,6 +82,19 @@ const Header = (props) => {
             Copy to Clipboard
           </Button>
           <br />
+          <Button
+            variant="contained"
+            className={styles.ModalButton}
+            onClick={() => {
+              downloadJson(config);
+              setOpenAlert(true);
+            }}
+          >
+            <a href={downloadJson(config)} download="config.json">
+              Download JSON File
+            </a>
+          </Button>
+          <br />
           <Collapse in={openAlert}>
             <Alert
               severity="success"
@@ -88,7 +112,7 @@ const Header = (props) => {
               }
               className={styles.ModalAlert}
             >
-              Copied to clipboard!
+              Success!
             </Alert>
           </Collapse>
 
